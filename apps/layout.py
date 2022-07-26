dfd0443f50e26cf724962673500b40db446ae2b2
Cory Latschkowski <koree@redhat.com>
2022-07-26 17:09:53 -0600

    update: wsgi

---
+++ apps/layout.py
@@ -0,0 +1,41 @@
+# used to display only
+from operator import mod
+from dash import dash, dcc, html
+
+# used for interactive callbacks
+from dash import Input, Output
+
+# pull in data to display
+from apps import model
+
+df = model.df
+
+import plotly.express as px
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
+])
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,7 @@
-# used to display only
-from dash import dash, dcc, html
-
-# used for interactive callbacks
-from dash import Input, Output
-
 # import app elements
-from apps import model
+from apps import layout
 
-df = model.df
-
-import plotly.express as px
-
-app = dash.Dash(__name__)
+app = layout.app
 
 # 'application' reference required for wgsi / gunicorn
 # https://docs.openshift.com/container-platform/3.11/using_images/s2i_images/python.html#using-images-python-configuration
@@ -24,31 +14,9 @@ application = app.server
 #     print(request.headers)
 #     print('=============== END HEADERS ===============')
 
-app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        df['year'].min(),
-        df['year'].max(),
-        step=None,
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        id='year-slider'
-    )
-])
-
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
-
-    fig.update_layout(transition_duration=500)
-
-    return fig
-
 if __name__ == "__main__":
     app.run_server(debug=True, port=8080, host="0.0.0.0")  # nosec
+
+# run gunicorn manually
+# TODO: move to readme
+# gunicorn wsgi:application -b 0.0.0.0:8080
